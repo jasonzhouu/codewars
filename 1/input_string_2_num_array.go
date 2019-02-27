42a1d6a5c0cf31ad56e40b9186bc2d218d9f4ef5
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-02-27 16:03:12 +0800

    将一个长函数拆成多个短小的函数

--- 1/input_string_2_num_array.go
+++ 1/input_string_2_num_array.go
@@ -4,20 +4,39 @@ import "fmt"
 import s "strings"
 import "strconv"
 
-func input2num_array(strg string) []int{
+func string2string_array(strg string) []string {
+	// "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"
+        // [01|15|59 1|47|16 01|17|20 1|32|34 2|17|17]
+        // 将第一行的格式转成第二行的格式
 	a := s.Split(strg, ", ")
-	a_len := len(a)
-	// b 双层slice用于存储时：分：秒 int 格式的列表
-	b := make([][3]int, a_len)
-	for i1, v1 := range a {
-		c := s.Split(v1, "|")
-		for i2, v2 := range c {
-			b[i1][i2], _ = strconv.Atoi(v2)
-		}
-	}
-	fmt.Println("input in int array:\t", b)
+//	fmt.Println(a)
+	return a
+}
+
+func string_to_2_level_int_slice(strg string) [][3]int {
+	// [01|15|59 1|47|16 01|17|20 1|32|34 2|17|17]
+	// [[1 15 59] [1 47 16] [1 17 20] [1 32 34] [2 17 17]]
+	// 将第一行的格式转成第二行的格式
+
+	a := string2string_array(strg)
+        // b 双层slice用于存储时：分：秒 int 格式的列表
+        b := make([][3]int, len(a))
+        for i1, v1 := range a {
+                c := s.Split(v1, "|")
+                for i2, v2 := range c {
+                        b[i1][i2], _ = strconv.Atoi(v2)
+                }
+        }
+//	fmt.Println(b)
+	return b
+}
+
+func string_to_seconds_slice(strg string) []int{
+
+	b := string_to_2_level_int_slice(strg)
+
 	// d 单层 slice 用于存储换算得到的秒钟数
-	d := make([]int, a_len)
+	d := make([]int, len(b))
         for i1, v1 := range b {
                 for i2, v2 := range v1 {
                         switch i2 {
@@ -36,5 +55,5 @@ func input2num_array(strg string) []int{
 
 func main() {
 	var a string = "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"
-	fmt.Println("input in seconds:\t", input2num_array(a))
+	fmt.Println(string_to_seconds_slice(a))
 }
