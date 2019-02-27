54648c81007716c65a6877e49159dff725489273
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2019-02-27 16:29:11 +0800

    以00|00|00的格式返回range
    - 将 input_string_2_num_array.go 拆小的函数融入main.go
    - 将seconds 格式的range转换成00|00|00格式并打印出来

--- 1/main.go
+++ 1/main.go
@@ -7,79 +7,87 @@ import s "strings"
 import "strconv"
 import "sort"
 
-func string2num(strg string) int{
-	string_array := s.Split(strg, "|")
-	var num_array [3]int
-	for i, v := range string_array {
-		num_array[i], _ = strconv.Atoi(v)
-	}
-	var time_in_seconds int
-	for i, v := range num_array {
-		switch i {
-		case 0:
-			time_in_seconds += v * 60 * 60
-		case 1:
-			time_in_seconds += v * 60
-		case 2:
-			time_in_seconds += v
-		}
-	}
-	return time_in_seconds
+func string2string_array(strg string) []string {
+	// "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"
+        // [01|15|59 1|47|16 01|17|20 1|32|34 2|17|17]
+        // 将第一行的格式转成第二行的格式
+	a := s.Split(strg, ", ")
+//	fmt.Println(a)
+	return a
 }
 
-func num2string(time_in_seconds int) string {
-        var num_array [3]int
-        for i := 2; i >= 0; i-- {
-                num_array[i] = time_in_seconds % 60
-                time_in_seconds = time_in_seconds / 60
-        }
-        var string_array [3]string
-        for i, v := range num_array {
-                if v < 10 {
-                        string_array[i] = "0" + strconv.Itoa(v)
-                        continue
+func string_to_2_level_int_slice(strg string) [][3]int {
+	// [01|15|59 1|47|16 01|17|20 1|32|34 2|17|17]
+	// [[1 15 59] [1 47 16] [1 17 20] [1 32 34] [2 17 17]]
+	// 将第一行的格式转成第二行的格式
+
+	a := string2string_array(strg)
+        // b 双层slice用于存储时：分：秒 int 格式的列表
+        b := make([][3]int, len(a))
+        for i1, v1 := range a {
+                c := s.Split(v1, "|")
+                for i2, v2 := range c {
+                        b[i1][i2], _ = strconv.Atoi(v2)
                 }
-                string_array[i] = strconv.Itoa(v)
         }
-        var time_in_string string
-        string_array2 := []string{string_array[0], string_array[1], string_array[2],}
-        for i := 2; i >= 0; i-- {
-                time_in_string = s.Join(string_array2, "|")
+//	fmt.Println(b)
+	return b
+}
+
+func string_to_seconds_slice(strg string) []int{
+
+	b := string_to_2_level_int_slice(strg)
+
+	// d 单层 slice 用于存储换算得到的秒钟数
+	d := make([]int, len(b))
+        for i1, v1 := range b {
+                for i2, v2 := range v1 {
+                        switch i2 {
+                                case 0:
+                                        d[i1] += v2 * 60 * 60
+                                case 1:
+                                        d[i1] += v2 * 60
+                                case 2:
+                                        d[i1] += v2
+                        }
+                }
         }
-        return time_in_string
+//	fmt.Println("input in seconds:\t", d)
+	return d
+}
+
+
+func num2string(time_in_seconds int)string {
+	num_array := make([]int, 3)
+	for i := 2; i >= 0; i-- {
+		num_array[i] = time_in_seconds % 60
+		time_in_seconds = time_in_seconds / 60
+	}
+	string_array := make([]string, 3)
+	for i, v := range num_array {
+		if v < 10 {
+			string_array[i] = "0" + strconv.Itoa(v)
+			continue
+		}
+		string_array[i] = strconv.Itoa(v)
+	}
+	var time_in_string string
+	for i := 2; i >= 0; i-- {
+		time_in_string = s.Join(string_array, "|")
+	}
+	return time_in_string
 }
 
 
 func main() {
 	var a string = "01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"
-	var b = s.Split(a, ", ")
-	var d [5][3]int
-	for i1, v := range b {
-		c := s.Split(v, "|")
-		for i2, v2 := range c {
-			d[i1][i2], _ = strconv.Atoi(v2)
-		}
-	}
-	fmt.Println("input in int array:\t", d)
-	var e [5]int
-	for i1, v1 := range d {
-		for i2, v2 := range v1 {
-			switch i2 {
-				case 0:
-					e[i1] += v2 * 60 * 60
-				case 1:
-					e[i1] += v2 * 60
-				case 2:
-					e[i1] += v2
-			}
-		}
-	}
-	fmt.Println("input in seconds:\t", e)
-	f := []int{e[0], e[1], e[2], e[3], e[4],}
-	sort.Ints(f)
-	fmt.Println("input in seconds sorted:\t", f)
-	min := f[0]
-	max := f[4]
+	seconds_slice := string_to_seconds_slice(a)
+	seconds_slice_sort := make([]int, 5)
+	copy(seconds_slice_sort, seconds_slice)
+	sort.Ints(seconds_slice_sort)
+	fmt.Println("input in seconds sorted:\t", seconds_slice_sort)
+	min := seconds_slice_sort[0]
+	max := seconds_slice_sort[4]
 	result_range := (max - min)
 	fmt.Println("range in seconds:\t", result_range)
 	fmt.Println("range in string:\t", num2string(result_range))
